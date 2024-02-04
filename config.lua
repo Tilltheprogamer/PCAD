03d30aba0ab47756d39575261592a032b2a93dec
Tilltheprogamer <118006357+Tilltheprogamer@users.noreply.github.com>
2024-02-04 01:36:49 +0100

    Update 1.1.0

--- config.lua
+++ config.lua
@@ -1,5 +1,7 @@
 Config = {}
 
+Config.NewESX = true      -- Use the New ESX Exports ? | Only for Version 1.8.5 and above.
+
 Config.Jobname = "police" -- enter your job here.
 
 Config.Open = "Command"  -- "Command" or "Key" possible
@@ -8,11 +10,11 @@ Config.OpenKey = nil -- nil = deactivated
 
 Config.OpenCommand = "pcad" -- /pcad like that
 
----------------------------------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 Config.ClientDebug = true       -- Debug for player data in the client F8 console / [triggers everytime by login]
 Config.ClientJobDebug = false    -- Debug for the setjob event from ESX will be shown in the client F8 console / [triggers everytime by job change]
 
 Config.ServerConsoleDebug = false    -- Debug for the server console / [triggers by connecting/disconnecting to the server]
 
----------------------------------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------
